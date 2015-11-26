54f6ab43188e9cb7870e412607ff74950a47ad86
Peter Jang <peter.x.jang@gmail.com>
2015-11-25 20:39:07 -0600

    created order in database (not all columns) and redirected to empty show page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,13 @@
 class OrdersController < ApplicationController
   def create
-    redirect_to "/orders/:id"
+    order = Order.create(
+      quantity: params[:form_input_quantity],
+      user_id: current_user.id
+    )
+    redirect_to "/orders/#{order.id}"
+  end
+
+  def show
+    
   end
 end
