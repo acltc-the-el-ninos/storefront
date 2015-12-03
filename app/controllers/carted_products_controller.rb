fcaf0afb4c17959b5364eafd45f315f0713346a5
Peter Jang <peter.x.jang@gmail.com>
2015-12-02 21:20:05 -0600

    redirected empty cart to index, added delete links on shopping cart page

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,6 +1,11 @@
 class CartedProductsController < ApplicationController
   def index
-    @carted_products = current_user.carted_products.where(status: "carted")
+    if current_user && current_user.carted_products.where(status: "carted").any?
+      @carted_products = current_user.carted_products.where(status: "carted")
+    else
+      flash[:warning] = "You don't have anything in your cart. Buy some stuff now!!!"
+      redirect_to "/"
+    end
   end
 
   def create
@@ -13,6 +18,13 @@ class CartedProductsController < ApplicationController
       quantity: quantity,
       status: "carted"
     )
+    redirect_to "/carted_products"
+  end
+
+  def destroy
+    carted_product = CartedProduct.find_by(id: params[:id])
+    carted_product.update(status: "removed")
+    flash[:success] = "Product successfully removed! Order something else though..."
     redirect_to "/carted_products"
   end
 end
