9deff3e3e47a88a689adeaa87504b3c35f6250fb
Peter Jang <peter.x.jang@gmail.com>
2015-12-02 20:42:27 -0600

    added calculations for creating an order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,25 @@
 class OrdersController < ApplicationController
   def create
-    
+    @carted_products = current_user.carted_products.where(status: "carted")
+
+    subtotal = 0
+    @carted_products.each do |carted_product|
+      quantity = carted_product.quantity
+      price = carted_product.product.price
+      subtotal += quantity * price
+    end
+
+    tax = subtotal * Product::SALES_TAX
+
+    total = subtotal + tax
+
+
+    order = Order.create(
+      user_id: current_user.id,
+      subtotal: subtotal,
+      tax: tax,
+      total: total
+    )
     redirect_to "/orders/#{order.id}"
   end
 
