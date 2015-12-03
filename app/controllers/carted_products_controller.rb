c114c925c69eaee398aaab3aec2036011464b944
Peter Jang <peter.x.jang@gmail.com>
2015-12-02 20:13:46 -0600

    created a carted_product

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,2 +1,14 @@
 class CartedProductsController < ApplicationController
+  def create
+    user_id = current_user.id
+    product_id = params[:form_input_product_id]
+    quantity = params[:form_input_quantity]
+    CartedProduct.create(
+      user_id: user_id,
+      product_id: product_id,
+      quantity: quantity,
+      status: "carted"
+    )
+    redirect_to "/carted_products"
+  end
 end
